8d61803d2cdeb51e7fb3f4ccae621624fd0acabd
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 <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>
2023-11-29 15:09:46 +0100

    ES6 promise

---
+++ ES6_promise/100-await.js
@@ -0,0 +1,19 @@
+import { createUser, uploadPhoto } from './utils';
+
+const asyncUploadUser = async () => {
+  let result = {
+    photo: null,
+    user: null,
+  };
+
+  try {
+    result = {
+      photo: await uploadPhoto(),
+      user: await createUser(),
+    };
+    return result;
+  } catch (error) {
+    return result;
+  }
+};
+export default asyncUploadUser;
