c6e3cf29aa1052d8809d7cbca53dcd5e9ea3b7f8
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 <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>
2023-11-29 18:42:15 +0100

    get list student

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 export default function getListStudentIds(list) {
-    if (!Array.isArray(list)) {
+  if (!Array.isArray(list)) {
     return [];
   }
   const listId = list.map((student) => student.id);
