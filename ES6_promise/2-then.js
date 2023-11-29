23a534a6cc15e008e780b8a0050fc8a24f7b5913
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 <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>
2023-11-29 15:20:16 +0100

    ES6_promise

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  const body = { status: 200, body: 'success' };
-  return promise
-    .then(() => body)
-    .catch((error) => error)
-    .finally(() => console.log('Got a response from the API'));
-}
+    return promise.then(() => ({ status: 200, body: 'success' }))
+      .catch(() => Error())
+      .finally(() => console.log('Got a response from the API'));
+  }
--- ES6_promise/README.md
+++ ES6_promise/README.md
@@ -1,1 +1,1 @@
-es6 promise
+README README
