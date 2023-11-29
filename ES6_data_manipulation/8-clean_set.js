94b81083f1e142b1c4dc2acd743ab4cb423a71fe
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 <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>
2023-11-29 18:58:47 +0100

    clean set

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+export default function cleanSet(set, startString) {
+  const result = []; 
+  if (typeof startString !== 'string' || typeof set !== 'object' || startString.length === 0) {
+     return '';
+  } 
+  for (const item of set) {
+    result.push(item.slice(startString.length));
+  }
+  return result.join('-');
+}
