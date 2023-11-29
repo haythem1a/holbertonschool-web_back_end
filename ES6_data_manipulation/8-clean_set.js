dea9547fba5d5348b0d888e6080c9fef56481906
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 <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>
2023-11-29 19:00:43 +0100

    clean set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
 export default function cleanSet(set, startString) {
-  const result = []; 
+  const result = [];
   if (typeof startString !== 'string' || typeof set !== 'object' || startString.length === 0) {
-     return '';
-  } 
+    return '';
+}
   for (const item of set) {
     result.push(item.slice(startString.length));
   }
