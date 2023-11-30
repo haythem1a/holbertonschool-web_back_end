8edb239508e0b4e503ed53170b75f2e850fa1fba
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 <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>
2023-11-30 12:28:02 +0100

    try

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -2,7 +2,7 @@ export default function guardrail(mathFunction) {
   const queue = [];
   try {
     const result = mathFunction();
-      queue.push(result);
+    queue.push(result);
   } catch (error) {
     queue.push(error.toString());
   } finally {
